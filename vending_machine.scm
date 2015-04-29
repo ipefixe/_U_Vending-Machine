85fc953c65cec0b9608b73faa047cb40410cab6b
Billy Everyteen <legauloisfr@gmail.com>
2015-04-29 07:34:58 +0200

    added functions: get sum of money inserted

--- vending_machine.scm
+++ vending_machine.scm
@@ -6,3 +6,15 @@
   (newline)
   x)
 
+(define (get-1euro l) (car l))
+(define (get-50cents l) (cadr l))
+(define (get-20cents l) (caddr l))
+(define (get-10cents l) (cadddr l))
+
+(define (sum-of-money l)
+  (+ (get-1euro l) (* (get-50cents l) 0.5) (* (get-20cents l) 0.2) (* (get-10cents l) 0.1)))
+
+;(define (give-change l num)
+;  (if (> num (sum-of-money l))
+;      #f
+; TODO
