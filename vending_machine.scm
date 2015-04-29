e7d8b417b8eac69912c971418ef088c55ecdb57f
Billy Everyteen <legauloisfr@gmail.com>
2015-04-29 12:32:30 +0200

    changes of give-back : back up if we can't do the give change

--- vending_machine.scm
+++ vending_machine.scm
@@ -7,7 +7,7 @@
   x)
 
 (define available-money '(10 10 10 10))
-(define as-before '())
+(define money-as-before '())
 
 (define (get-1euro l) (car l))
 (define (get-50cents l) (cadr l))
@@ -19,7 +19,7 @@
 
 (define (give-change l num)
   (let ((diff (- (sum-of-money l) num)))
-    (if (zero? diff)
+    (if (<= diff 0)
         #f
         (begin
           (set! available-money (list (+ (get-1euro available-money) (get-1euro l))
@@ -29,26 +29,34 @@
           (give-back diff)))))
 
 (define (give-back n)
-  (let give-back-loop ((l-return '(0 0 0 0))
+  (let give-back-loop ((copy-available-money available-money)
+                       (l-return '(0 0 0 0))
                        (num n))
     (if (zero? num)
         (begin
           (display "Give change finished")(newline)
+          (set! available-money copy-available-money)
           l-return)
-        (if (and (>= num 100) (> (get-1euro available-money) 0))
-            (begin
-              (set! available-money (list (- (get-1euro available-money) 1) (get-50cents available-money) (get-20cents available-money) (get-10cents available-money)))
-              (give-back-loop (list (+ (get-1euro l-return) 1) 0 0 0) (- num 100)))
-            (if (and (>= num 50) (> (get-50cents available-money) 0))
-                (begin
-                  (set! available-money (list (get-1euro available-money) (- (get-50cents available-money) 1) (get-20cents available-money) (get-10cents available-money)))
-                  (give-back-loop (list (get-1euro l-return) (+ (get-50cents l-return) 1) 0 0) (- num 50)))
-                (if (and (>= num 20) (> (get-20cents available-money) 0))
-                    (begin
-                      (set! available-money (list (get-1euro available-money) (get-50cents available-money) (- (get-20cents available-money) 1) (get-10cents available-money)))
-                      (give-back-loop (list (get-1euro l-return) (get-50cents l-return) (+ (get-20cents l-return) 1) 0) (- num 20)))
-                    (if (and (>= num 10) (> (get-10cents available-money) 0))
-                        (begin
-                          (set! available-money (list (get-1euro available-money) (get-50cents available-money) (get-20cents available-money) (- (get-10cents available-money) 1)))
-                          (give-back-loop (list (get-1euro l-return) (get-50cents l-return) (get-20cents l-return) (+ (get-10cents l-return) 1)) (- num 10)))
-                        (display "Not enough money to give back"))))))))
+        (if (and (>= num 100) (> (get-1euro copy-available-money) 0))
+            (give-back-loop (list (- (get-1euro copy-available-money) 1) (get-50cents copy-available-money) (get-20cents copy-available-money) (get-10cents copy-available-money))
+                            (list (+ (get-1euro l-return) 1) 0 0 0) 
+                            (- num 100))
+            (if (and (>= num 50) (> (get-50cents copy-available-money) 0))
+                (give-back-loop (list (get-1euro copy-available-money) (- (get-50cents copy-available-money) 1) (get-20cents copy-available-money) (get-10cents copy-available-money))
+                                (list (get-1euro l-return) (+ (get-50cents l-return) 1) 0 0) 
+                                (- num 50))
+                (if (and (>= num 20) (> (get-20cents copy-available-money) 0))
+                    (give-back-loop (list (get-1euro copy-available-money) (get-50cents copy-available-money) (- (get-20cents copy-available-money) 1) (get-10cents copy-available-money))
+                                    (list (get-1euro l-return) (get-50cents l-return) (+ (get-20cents l-return) 1) 0) 
+                                    (- num 20))
+                    (if (and (>= num 10) (> (get-10cents copy-available-money) 0))
+                        (give-back-loop (list (get-1euro copy-available-money) (get-50cents copy-available-money) (get-20cents copy-available-money) (- (get-10cents copy-available-money) 1))
+                                        (list (get-1euro l-return) (get-50cents l-return) (get-20cents l-return) (+ (get-10cents l-return) 1))
+                                        (- num 10))
+                        (begin 
+                          (display "Not enough money to give back")
+                          #f))))))))
+
+;(define (vending_machine drink money)
+;  (if (< (sum-of-money money) 500) ; ou pouvons nous faire le change si la somme est supérieur ?
+  
