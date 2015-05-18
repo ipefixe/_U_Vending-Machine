23ac37e53237a1d1c78093f997704b54c6daa7b3
kevbp <legauloisfr@gmail.com>
2015-05-18 15:53:30 +0200

    finished

--- vending_machine_without_global_variables.scm
+++ vending_machine_without_global_variables.scm
@@ -21,7 +21,11 @@
 (define (give-change l num available-money)
   (let ((diff (- (sum-of-money l) num)))
     (if (<= diff 0)
-        #f
+        (list (list (+ (get-1euro available-money) (get-1euro l))
+                                      (+ (get-50cents available-money) (get-50cents l))
+                                      (+ (get-20cents available-money) (get-20cents l))
+                                      (+ (get-10cents available-money) (get-10cents l))) 
+              #t)
         (begin
           (set! available-money (list (+ (get-1euro available-money) (get-1euro l))
                                       (+ (get-50cents available-money) (get-50cents l))
@@ -37,8 +41,7 @@
         (begin
           (display "Give change finished")(newline)
           (display l-return)(newline)
-          (set! available-money copy-available-money)
-          l-return)
+          (list copy-available-money l-return))
         (if (and (>= num 100) (> (get-1euro copy-available-money) 0))
             (give-back-loop (list (- (get-1euro copy-available-money) 1) (get-50cents copy-available-money) (get-20cents copy-available-money) (get-10cents copy-available-money))
                             (list (+ (get-1euro l-return) 1) 0 0 0) 
@@ -57,14 +60,14 @@
                                         (- num 10))
                         (begin 
                           (display "Not enough money to give back")(newline)
-                          #f))))))))
+                          (list available-money #f)))))))))
 
 (define vending-machine
   (let ((available-drink '(30 1 5))
         (available-money '(0 10 10 10))
         (money-as-before '()))
     (lambda (drink money)
-      (display "Available drink ")(display available-drink)(newline)
+      (newline)(display "Available drink ")(display available-drink)(newline)
       (display "Available money ")(display available-money)(newline)
       (cond ((equal? money 'as-before) (if (null? money-as-before)
                                            #f
@@ -74,39 +77,66 @@
                (set! money-as-before '())
                #f))
             (else
-             (cond ((equal? drink 'black-coffee) (if (zero? (get-black-coffee available-drink))
-                                                     (begin 
-                                                       (set! money-as-before money)
-                                                       #f)
-                                                     (begin
-                                                       (set! available-drink (list (- (get-black-coffee available-drink) 1) (get-white-coffee available-drink) (get-hot-chocolate available-drink)))
-                                                       (set! money-as-before '())
-                                                       (give-change money 50 available-money)
-                                                       (display "Black-coffee")(newline)(newline))))
-                   ((equal? drink 'white-coffee) (if (zero? (get-white-coffee available-drink))
-                                                     (begin
-                                                       (set! money-as-before money)
-                                                       #f)
-                                                     (begin
-                                                       (set! available-drink (list (get-black-coffee available-drink) (- (get-white-coffee available-drink) 1) (get-hot-chocolate available-drink)))
-                                                       (set! money-as-before '())
-                                                       (give-change money 50 available-money)
-                                                       (display "White-coffee")(newline)(newline))))
-                   ((equal? drink 'hot-chocolate) (if (zero? (get-hot-chocolate available-drink))
-                                                      (begin
-                                                        (set! money-as-before money)
-                                                        #f)
-                                                      (begin
-                                                        (set! available-drink (list (get-black-coffee available-drink) (get-white-coffee available-drink) (- (get-hot-chocolate available-drink) 1)))
-                                                        (set! money-as-before '())
-                                                        (give-change money 50 available-money)
-                                                        (display "Hot chocolate")(newline)(newline))))
-                   (else (begin
-                           (set! money-as-before money)
-                           #f))))))))
-
+             (call/cc (lambda (exit)
+                        (cond ((equal? drink 'black-coffee) (if (zero? (get-black-coffee available-drink))
+                                                                (begin 
+                                                                  (set! money-as-before money)
+                                                                  #f)
+                                                                (begin
+                                                                  (let ((tmp '()))
+                                                                    (set! tmp (give-change money 50 available-money))
+                                                                    (if (equal? (cadr tmp) #f)
+                                                                        (begin
+                                                                          (set! tmp (give-back (sum-of-money money) available-money))
+                                                                          (set! available-money (car tmp))
+                                                                          (exit #f))
+                                                                        (set! available-money (car tmp)))
+                                                                    (set! available-drink (list (- (get-black-coffee available-drink) 1) (get-white-coffee available-drink) (get-hot-chocolate available-drink)))
+                                                                    (set! money-as-before '())
+                                                                    (cdr tmp))
+                                                                  (display "Black-coffee")(newline))))
+                              ((equal? drink 'white-coffee) (if (zero? (get-white-coffee available-drink))
+                                                                (begin
+                                                                  (set! money-as-before money)
+                                                                  #f)
+                                                                (begin
+                                                                  (let ((tmp '()))
+                                                                    (set! tmp (give-change money 50 available-money))
+                                                                    (if (equal? (cadr tmp) #f)
+                                                                        (begin
+                                                                          (set! tmp (give-back (sum-of-money money) available-money))
+                                                                          (set! available-money (car tmp))
+                                                                          (exit #f))
+                                                                        (set! available-money (car tmp)))
+                                                                    (set! available-drink (list (get-black-coffee available-drink) (- (get-white-coffee available-drink) 1) (get-hot-chocolate available-drink)))
+                                                                    (set! money-as-before '())
+                                                                    (cdr tmp))
+                                                                  (display "White-coffee")(newline))))
+                              ((equal? drink 'hot-chocolate) (if (zero? (get-hot-chocolate available-drink))
+                                                                 (begin
+                                                                   (set! money-as-before money)
+                                                                   #f)
+                                                                 (begin
+                                                                   (let ((tmp '()))
+                                                                     (set! tmp (give-change money 50 available-money))
+                                                                     (if (equal? (cadr tmp) #f)
+                                                                        (begin
+                                                                          (set! tmp (give-back (sum-of-money money) available-money))
+                                                                          (set! available-money (car tmp))
+                                                                          (exit #f))
+                                                                        (set! available-money (car tmp)))
+                                                                     (set! available-drink (list (get-black-coffee available-drink) (get-white-coffee available-drink) (- (get-hot-chocolate available-drink) 1)))
+                                                                     (set! money-as-before '())
+                                                                     (cdr tmp))
+                                                                   (display "Hot chocolate")(newline))))
+                              (else (begin
+                                      (set! money-as-before money)
+                                      #f))))))))))
+    
 (define (run)
   (vending-machine 'black-coffee '(1 0 0 0))
   (vending-machine 'black-coffee '(1 0 0 0))
-  (vending-machine 'hot-chocolate '(1 0 0 0))
-  (vending-machine 'hot-chocolate '(1 0 0 0)))
+  (vending-machine 'hot-chocolate '(0 1 0 0))
+  (vending-machine 'hot-chocolate '(0 1 0 0))
+  (vending-machine 'hot-chocolate '(0 0 3 0))
+  (vending-machine 'hot-chocolate '(0 1 0 0)))
